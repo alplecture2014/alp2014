201b34575a408b2331abf1b137e020f67d387c35
nagikasa <narges.kasaeizadeh@gmail.com>
2014-04-15 19:08:44 +0100

    Task 3 and Task 4 screenshots and availability

--- timetable/student_availability.pl
+++ timetable/student_availability.pl
@@ -423,3 +423,33 @@ unavailable('Sareh Ghayoomi', wednesday, 15).
 unavailable('Sareh Ghayoomi', wednesday, 16).        
 unavailable('Sareh Ghayoomi', wednesday, 17).        
 unavailable('Sareh Ghayoomi', wednesday, 18). 
+
+
+unavailable( 'Narges Kasaeizadeh', monday, 16 ).
+unavailable( 'Narges Kasaeizadeh', monday, 17 ).
+unavailable( 'Narges Kasaeizadeh', tuesday, 10 ).
+unavailable( 'Narges Kasaeizadeh', tuesday, 11 ).
+unavailable( 'Narges Kasaeizadeh', tuesday, 13 ).
+unavailable( 'Narges Kasaeizadeh', tuesday, 14 ).
+unavailable( 'Narges Kasaeizadeh', wednesday, 16 ).
+unavailable( 'Narges Kasaeizadeh', wednesday, 17 ).
+unavailable( 'Narges Kasaeizadeh', wednesday, 18 ).
+unavailable( 'Narges Kasaeizadeh', thursday, 13 ).
+unavailable( 'Narges Kasaeizadeh', thursday, 14 ).
+unavailable( 'Narges Kasaeizadeh', thursday, 15 ).
+unavailable( 'Narges Kasaeizadeh', Friday, 9 ).
+unavailable( 'Narges Kasaeizadeh', Friday, 10 ).
+unavailable( 'Narges Kasaeizadeh', Friday, 11 ).
+unavailable( 'Narges Kasaeizadeh', Friday, 14 ).
+unavailable( 'Narges Kasaeizadeh', Friday, 15 ).
+unavailable( 'Narges Kasaeizadeh', Friday, 16 ).
+unavailable( 'Narges Kasaeizadeh', Friday, 17 ).
+
+
+
+
+
+
+
+
+
